eaac4d0ec8c79fb61c040382cb5e78793647b952
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-02 15:14:25 +0530

    feat: Add SearchInput component for search functionality

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const SearchInput = () => {
+  const [term, setTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    navigate(`/search?term=${term}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="search-term"
+        id="search-term"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default SearchInput;
