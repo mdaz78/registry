8d678e842b09f855ca1b9e2e3c9b73ddb273e79d
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-02 14:48:18 +0530

    create the routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import DetailsPage from "./pages/DetailsPage";
+import HomePage from "./pages/HomePage";
+import Root from "./pages/Root";
+import SearchPage from "./pages/SearchPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/packages/:name",
+        element: <DetailsPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      <h1>I am running</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
